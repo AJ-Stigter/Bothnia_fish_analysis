73f9119032853099c7cb29729c06356d9a626817
AJ-Stigter <adriaenstigter@live.nl>
2025-08-19 21:00:48 +0200

    Added read me file for instructions. Date: 19/08/2025, by: Adriaen Stigter

---
+++ Scripts/Read_me_instructions.R
@@ -0,0 +1,17 @@
+# Read me file!
+
+# To recreate all steps, start in 1. Exploration
+# First all fish data was  merged in: "Merge_fish_data"
+# Then the fish data was explored and cleaned in: "Explore_and clean_fish_data"
+# After this the fish data was merged with the different environmental datasets
+#         For temperature and salinity: "Merge_temp_sal_fish"
+#         For chla: "Merge_chla_fish"
+#         For other environmental variables such as nutrients: "Merge_dbotnia_fish"
+
+
+# To recreate analysis (GAM), start in 2. Analysis
+# First timeseries was done in: "GAM_timeseries"
+# Then all predictor variables were analysed
+#         For temperature and salinity: "GAM_temp_sal"
+#         For chla: "GAM_chla"
+#         For other environmental variables such as nutrients: "GAM_dbo"
